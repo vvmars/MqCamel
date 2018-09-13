467d07e2808697bf1a1cd933a0ed277d6fc932c9
vladimir.samchinski <vladimir.samchinskiy@ihsmarkit.com>
2018-09-13 19:24:32 +0300

    -

--- src/main/java/com/DataRouter.java
+++ src/main/java/com/DataRouter.java
@@ -1,8 +1,15 @@
 package com;
 
 import org.springframework.stereotype.Component;
+import org.apache.camel.builder.RouteBuilder;
 
 @Component
-public class DataRouter {
+public class DataRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+        from("QUEUE" + "?concurrentConsumers=" + "CONSUMER_THREADS")
+            .log("Start: correlation_id=${header.MCP_CORRELATION_ID}, action=validate1000")
+            .setHeader("INCOMING_QUEUE", constant("1000_0409")).to("direct:process-validation");
 
 }
