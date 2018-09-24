48367bbdbc7b39dfee7581cd5da0e28c49ef79fe
vladimir.samchinski <vladimir.samchinskiy@ihsmarkit.com>
2018-09-24 19:39:55 +0300

    -

--- src/main/java/com/Application.java
+++ src/main/java/com/Application.java
@@ -1,6 +1,8 @@
 package com;
 
 //import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -8,11 +10,16 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class Application {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         //SpringApplicationBuilder springBuilder = new SpringApplicationBuilder(Application.class);
         //springBuilder.run(args);
         ApplicationContext context = new AnnotationConfigApplicationContext(Configuration.class);
         Connection con = context.getBean(Connection.class);
+        CamelContext context1 = new DefaultCamelContext();
+        context1.addRoutes(new DataRouter());
+        context1.start();
+        Thread.sleep(30000);
+        context1.stop();
 
     }
 }
--- src/main/java/com/DataRouter.java
+++ src/main/java/com/DataRouter.java
@@ -8,8 +8,12 @@ public class DataRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        from("QUEUE" + "?concurrentConsumers=" + "CONSUMER_THREADS")
-                .log("Start: correlation_id=${header.MCP_CORRELATION_ID}, action=validate1000")
-                .setHeader("INCOMING_QUEUE", constant("1000_0409")).to("direct:process-validation");
+//        from("QUEUE" + "?concurrentConsumers=" + "CONSUMER_THREADS")
+//                .log("Start: correlation_id=${header.MCP_CORRELATION_ID}, action=validate1000")
+//                .setHeader("INCOMING_QUEUE", constant("1000_0409")).to("direct:process-validation");
+        from("file:src/main/resources/in?autoCreate=false")
+                .log("@@@test@@@")
+                .convertBodyTo(String.class)
+                .to("file:src/main/resources/out?autoCreate=false");
     }
 }
